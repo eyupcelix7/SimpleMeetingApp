41dbf700880c0891dc13177a4ac84ece90a4c6f8
Eyüp Çelik <eyupturkey641@gmail.com>
2025-11-10 01:20:05 +0300

    HTTP Verbs'ler ve Tag Helpers'lar Eklendi

--- Controllers/MeetingController.cs
+++ Controllers/MeetingController.cs
@@ -1,19 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleMeetingApp.Models;
 
 namespace SimpleMeetingApp.Controllers
 {
     public class MeetingController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpGet]
         public IActionResult Apply()
         {
             return View();
         }
 
+        [HttpPost]
+        public IActionResult Apply(UserInfo user)
+        {
+            Console.WriteLine($"Adı: {user.Name}, Telefon Numarası: {user.Phone}, Email Adresi: {user.Email}, Katılım Durumu: {user.KatilimDurumu.ToString()}");
+            return View();
+        }
+
+        [HttpGet]
         public IActionResult List()
         {
             return View();
