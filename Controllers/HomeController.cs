0a88910951a193c6f87a893e949eec8bb27cbf49
Eyüp Çelik <eyupturkey641@gmail.com>
2025-11-09 23:22:02 +0300

    Models'ler Eklendi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SimpleMeetingApp.Models;
 
 namespace SimpleMeetingApp.Controllers
 {
@@ -6,7 +7,22 @@ namespace SimpleMeetingApp.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            int saat = DateTime.Now.Hour;
+
+            //ViewBag.Selamla = saat > 12 ? "İyi Günlers" : "Günaydın";
+            //ViewBag.Username = "eyupcelix7";
+            ViewData["Selamla"] = saat > 12 ? "İyi günler aga" : "Günaydın hacı";
+            ViewData["Username"] = "eyupcelix7";
+
+            MeetingInfo info = new MeetingInfo
+            {
+                Id = 1,
+                Location = "Istanbul",
+                Date = DateTime.Now,
+                NumberOfPeople = 4
+            };
+
+            return View(model: info);
         }
     }
 }
