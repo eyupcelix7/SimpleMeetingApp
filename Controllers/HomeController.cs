2628ae616369070f61a83c2d5d9398680663f9ee
Eyüp Çelik <eyupturkey641@gmail.com>
2025-11-09 17:28:09 +0300

    Controller'lar eklendi

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleMeetingApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Selamun Aleyküm";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapDefaultControllerRoute();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
 
 app.Run();
